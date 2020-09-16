ec49e419cd7255bc8e13158ecd6ee4292e243787
Ujjwal Mishra <ujjwal@scroll.in>
2020-09-16 09:50:41 +0530

    new build

--- dist/main.umd.js
+++ dist/main.umd.js
@@ -212,18 +212,22 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"c2ecb1c6-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/AudioPlayer.vue?vue&type=template&id=083af2ac&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"c2ecb1c6-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/AudioPlayer.vue?vue&type=template&id=1686ccb6&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('audio',{ref:"audioEl",attrs:{"src":_vm.src},on:{"loadedmetadata":_vm.onLoadedMetaData,"loadeddata":_vm.onLodedData,"timeupdate":_vm.onTimeUpdate,"play":_vm.onPlay,"playing":_vm.onPlaying,"waiting":_vm.onBuffering,"pause":_vm.onPause,"ended":_vm.onEnded,"error":_vm.onError}}),_c('div',{ref:"playerEl",class:[
       'audio-player',
       _vm.paused ? '' : 'audio-player-playing',
       _vm.disabled ? 'audio-player-disabled' : ''
-    ],on:{"click":_vm.handleMouseClick,"mousemove":_vm.handleMouseMove,"touchmove":_vm.handleMouseMove,"mousedown":_vm.handleMouseDown,"touchstart":_vm.handleMouseDown,"mouseup":_vm.handleMouseUp,"mouseleave":_vm.handleMouseUp,"touchend":_vm.handleMouseUp,"touchcancel":_vm.handleMouseUp}},[_c('div',{staticClass:"player-button gold-button"},[_c('i',{class:['icon', _vm.paused ? 'audio-play-icon' : 'audio-pause-icon']})]),_c('div',{ref:"currentTimeEl",staticClass:"player-time player-current-time"},[_vm._v(" "+_vm._s(_vm.currentTimeText)+" ")]),_c('div',{ref:"playerSeekbarEl",staticClass:"player-seekbar"},[_c('div',{ref:"progressEl",class:['progress', _vm.buffering ? 'indeterminate-progress' : '']},[_c('div',{ref:"progressDotEl",staticClass:"progress-pin"}),_c('div',{ref:"progressBarEl",staticClass:"progress-bar",staticStyle:{"width":"0"},attrs:{"role":"progressbar","aria-valuemin":"0","aria-valuemax":"100"}})])]),_c('div',{ref:"endTimeEl",staticClass:"player-time player-end-time"},[_vm._v(" "+_vm._s(_vm.endTimeText)+" ")])])])}
+    ],on:{"click":_vm.handleMouseClick,"mousemove":_vm.handleMouseMove,"touchmove":_vm.handleMouseMove,"mousedown":_vm.handleMouseDown,"touchstart":_vm.handleMouseDown,"mouseup":_vm.handleMouseUp,"mouseleave":_vm.handleMouseUp,"touchend":_vm.handleMouseUp,"touchcancel":_vm.handleMouseUp}},[_c('div',{ref:"playButton",staticClass:"player-button gold-button"},[_c('i',{class:['icon', _vm.paused ? 'audio-play-icon' : 'audio-pause-icon']})]),_c('div',{ref:"currentTimeEl",staticClass:"player-time player-current-time"},[_vm._v(" "+_vm._s(_vm.currentTimeText)+" ")]),_c('div',{ref:"playerSeekbarEl",staticClass:"player-seekbar"},[_c('div',{ref:"progressEl",class:['progress', _vm.buffering ? 'indeterminate-progress' : '']},[_c('div',{directives:[{name:"show",rawName:"v-show",value:(!_vm.buffering),expression:"!buffering"}],ref:"progressDotEl",staticClass:"progress-pin"}),_c('div',{ref:"progressBarEl",staticClass:"progress-bar",staticStyle:{"width":"0"},attrs:{"role":"progressbar","aria-valuemin":"0","aria-valuemax":"100"}})])]),_c('div',{ref:"endTimeEl",staticClass:"player-time player-end-time"},[_vm._v(" "+_vm._s(_vm.endTimeText)+" ")])])])}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/AudioPlayer.vue?vue&type=template&id=083af2ac&
+// CONCATENATED MODULE: ./src/components/AudioPlayer.vue?vue&type=template&id=1686ccb6&
 
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/AudioPlayer.vue?vue&type=script&lang=js&
+//
+//
+//
+//
 //
 //
 //
@@ -366,7 +370,8 @@ let mousedown = false;
       const {
         currentTimeEl,
         endTimeEl,
-        audioEl
+        audioEl,
+        playButton
       } = this.$refs;
       const ignoreList = [currentTimeEl, endTimeEl];
       e.preventDefault();
@@ -374,7 +379,7 @@ let mousedown = false;
       if (this.disabled) return;
       if (this.isNearSeekBar(e) || ignoreList.indexOf(e.target) !== -1) return;
       if (audioEl.readyState <= 1) this.buffering = true;
-      this.paused = !this.paused;
+      if (playButton.contains(e.target)) this.paused = !this.paused;
     },
 
     onTimeUpdate() {
@@ -469,6 +474,9 @@ let mousedown = false;
 
     onEnded() {
       this.paused = true;
+      this.currentTime = 0;
+      this.$refs.audioEl.currentTime = 0;
+      this.handleProgess();
     },
 
     onBuffering() {
@@ -479,10 +487,6 @@ let mousedown = false;
       this.paused = true;
       this.buffering = false;
       this.$emit("error", e);
-    },
-
-    getHnalder(handler) {
-      if (this.disabled) () => {};else handler;
     },
 
     isNearSeekBar(e) {
